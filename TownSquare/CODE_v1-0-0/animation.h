f1bd5c6f544e368322b75bd76555f05dd694fc05
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2023-11-15 00:56:21 +0100

    Removed v1.0.0 code files.

--- TownSquare/CODE_v1-0-0/animation.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// We called this function from animation.cpp. Remember to use the instruction: AnimatedTextures(); in the OnFrame.
-
-	void AnimatedTextures();
--- TownSquare/CODE_v1-0-0/startpos.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Individual StartPosition values - Since all characters will use the same one for this stage, I can only write 1 global definition for each act and call it individually for each character. Use the instruction: helperFunctions.RegisterStartPosition(Characters_X, SpeedHighwayX); in the Init.
-	StartPosition SpeedHighway0 = { LevelIDs_SpeedHighway, 0, { 3975, -1500, 4790 }, 0xC000 };
-	StartPosition SpeedHighway1 = { LevelIDs_SpeedHighway, 1, { 572, -19220, 0 }, 0xC000 };
-	StartPosition SpeedHighway2 = { LevelIDs_SpeedHighway, 2, { 152.2f, 0, 342.48f }, 0xC000 };
