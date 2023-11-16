48f78018af42e2c65cf76f54d20dc5f14b2e7572
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2023-11-16 01:25:48 +0100

    Added programming and config files.

--- TownSquare/pch.h
+++ TownSquare/pch.h
@@ -1,6 +1,27 @@
 #ifndef PCH_H
 #define PCH_H
 
-#include "framework.h"
+	#include "framework.h"
+
+	#include "SADXModLoader.h"
+
+	#include "FunctionHook.h"
+	#include "Trampoline.h"
+	#include "UsercallFunctionHandler.h"
+
+	#include "ModelInfo.h"
+	#include "LandTableInfo.h"
+	#include "AnimationFile.h"
+
+	#include "IniFile.hpp"
+
+	#include "mod.h"
+
+	#include "graphics.h"
+	#include "level.h"
+	#include "leveltask.h"	
+	#include "levelranks.h"
+	#include "objects.h"
+	#include "utils.h"
 
 #endif
